48615dbd28c2b3646ac76ad0573c4711fb212fcf
Spec27 <shkvara27@gmail.com>
2022-11-25 14:16:36 +0200

    test 3

--- src/App.js
+++ src/App.js
@@ -3,7 +3,10 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <h1>Hello World it is my portfolio konstruktor</h1>
+        <h1>
+          Hello World it is my portfolio konstruktor. It is site developmen for
+          my needs
+        </h1>
       </header>
     </div>
   );
